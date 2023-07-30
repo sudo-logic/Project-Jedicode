03c61667a808836da8fee497c1f543558dc8247c
Sitanshu Pokalwar <sitz2309@gmal.com>
2023-07-30 17:42:14 +0530

    🚨 changing client

--- client/src/env.ts
+++
@@ -1,7 +0,0 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
-
-// Cloudinary details
-export const CLOUDINARY_URL =
-  'https://api.cloudinary.com/v1_1/dzsifr04l/upload';
-export const CLOUDINARY_UPLOAD_PRESET = 'troubleshooting';
-export const CLOUDINARY_API_KEY = '';
