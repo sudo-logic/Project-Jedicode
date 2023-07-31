be7bdb1218853bf8e2efbf2684e7581639b12fd1
Ayush Mishra <awesomelogix@gmail.com>
2023-08-01 00:10:42 +0530

    implemented websockets

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,27 +1,31 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ormconfig } from './orm.config';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { QuestionsController } from './questions/questions.controller';
-import { QuestionsModule } from './questions/questions.module';
-import { SubmissionsModule } from './submissions/submissions.module';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {AuthModule} from './auth/auth.module';
+import {ChatGateway} from './chat.gateway';
+import {ormconfig} from './orm.config';
+import {QuestionsController} from './questions/questions.controller';
+import {QuestionsModule} from './questions/questions.module';
+import {SubmissionsModule} from './submissions/submissions.module';
+import {UsersModule} from './users/users.module';
+
 // const ormconfig = require('./orm.config')
 require('dotenv').config({
-  path: `./${process.env.NODE_ENV}.env`,
+  path : `./${process.env.NODE_ENV}.env`,
 });
 
 @Module({
-  imports: [
+  imports : [
     TypeOrmModule.forRoot(ormconfig),
     UsersModule,
     AuthModule,
     QuestionsModule,
     SubmissionsModule,
   ],
-  controllers: [AppController, QuestionsController],
-  providers: [AppService],
+  controllers : [ AppController, QuestionsController ],
+  providers : [ AppService, ChatGateway ],
 })
-export class AppModule {}
+export class AppModule {
+}
