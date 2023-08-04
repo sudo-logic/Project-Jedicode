d1e6c9c0588a6020d48eef4f72c6b614629a22eb
Sitanshu Pokalwar <sitz2309@gmail.com>
2023-08-04 17:03:42 +0530

    implement timer

---
+++ client/src/components/QTimer.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useTimer } from "react-timer-hook";
+
+export default function QTimer({ expiryTimestamp }) {
+  const { seconds, minutes, isRunning, start, pause, resume, restart } =
+    useTimer({
+      expiryTimestamp,
+      autoStart: true,
+      onExpire: () => console.warn("onExpire called"),
+    });
+
+  return (
+    <div className="rounded-md bg-dark-layer-2 flex gap-3 text-white items-center px-2">
+      <div className="text-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </div>
+      <div className=" pt-[2px] text-dark-label-2 text-sm font-semibold">
+        <span>{minutes}</span> : <span>{seconds}</span>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/Workspace/Workspace.jsx
+++ client/src/pages/Workspace/Workspace.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import Split from "react-split";
 import Problem from "./Problem";
 import Editor from "./Editor";
-import Room from "../Room";
+import QTimer from "../../components/QTimer";
+import Room from "../../components/Room";
 
 function Workspace() {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 1800);
+
   return (
     <>
       <Split className="split max-h-screen p-[10px] overflow-hidden bg-[rgb(26,26,26)]">
         <Problem />
         <Editor />
       </Split>
-      <div className="p-[22px] flex justify-end absolute right-0 top-0">
-        {" "}
+      <div className="p-[22px] flex justify-between gap-3 absolute right-0 top-0">
+        <QTimer expiryTimestamp={time} />
         <Room />
-        {/* <Timer/> */}
       </div>
     </>
   );
