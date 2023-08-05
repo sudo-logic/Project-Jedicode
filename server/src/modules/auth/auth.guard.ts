6e865bcb7c5ffb56baa6ef6a072cfbec64db24ea
Ayush Mishra <awesomelogix@gmail.com>
2023-08-06 01:53:45 +0530

    test gitlab-ci
    Update .gitlab-ci.yml file
    
    test gitlab-ci
    
    Update .gitlab-ci.yml
    
    test gitlab-ci
    
    test server gitlab-ci
    
    test server gitlab-ci
    
    test server gitlab-ci
    
    test server gitlab-ci
    
    update package.json
    
    update package.json
    
    update package.json
    
    update gitlab ci
    
    update gitlab ci

--- server/src/modules/auth/auth.guard.ts
+++ server/src/modules/auth/auth.guard.ts
@@ -12,7 +12,10 @@ import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService, private reflector: Reflector) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
