bf83554cce860972e62c273d524a5fc4b69f2d89
Ayush Mishra <awesomelogix@gmail.com>
2023-08-06 15:24:43 +0530

    feat: working websockets

--- client/src/toBeShifted/socket.js
+++ client/src/toBeShifted/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 export const initSocket = async () => {
   const options = {
-    "force new connection": true,
+    "force new connection": false,
     reconnectionAttempt: "Infinity",
     timeout: 10000,
     transports: ["websocket"],
