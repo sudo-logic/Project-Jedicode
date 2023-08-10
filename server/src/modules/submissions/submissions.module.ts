5daa2b80947176f592dd7cf54ddd8a0931aeadd5
Ayush Mishra <awesomelogix@gmail.com>
2023-08-10 15:42:05 +0530

    fix formatting

--- server/src/modules/submissions/submissions.module.ts
+++ server/src/modules/submissions/submissions.module.ts
@@ -3,7 +3,6 @@ import { SubmissionsController } from './submissions.controller';
 import { SubmissionsService } from './submissions.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Submission } from './submissions.entity';
-import { RunnerService } from '../runner/runner.service';
 import { RunnerModule } from '../runner/runner.module';
 @Module({
   imports: [TypeOrmModule.forFeature([Submission]), RunnerModule],
