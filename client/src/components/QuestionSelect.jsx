ece20bd819ce39a8055b32b89112f2901d50a27d
Ayush Mishra <awesomelogix@gmail.com>
2023-08-11 16:36:19 +0530

    dynamic questions select

--- client/src/components/QuestionSelect.jsx
+++ client/src/components/QuestionSelect.jsx
@@ -1,22 +1,25 @@
 import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import axios from "axios";
 
-const questions = [
-  { name: "Q1. Two Sum" },
-  { name: "Q2. Lorem Ipsum" },
-  { name: "Q3. Dolor sit amet" },
-];
+const questions = await axios
+  .get("http://34.100.255.183/questions/random/3")
+  .then((res) => {
+    return res.data;
+  });
+
+console.log(questions);
 
 export default function QuestionSelect() {
   const [selected, setSelected] = useState(questions[0]);
 
   return (
-    <div className="bg-dark-layer-2 m-3 text-xs cursor-pointer font-medium rounded-md w-56">
+    <div className="bg-dark-layer-2 m-3 text-xs cursor-pointer font-medium rounded-md w-auto">
       <Listbox value={selected} onChange={setSelected}>
         <div className="relative mt-1">
           <Listbox.Button className="relative text-white w-full cursor-default rounded-md py-2 pl-4 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-            <span className="block truncate font-bold">{selected.name}</span>
+            <span className="block truncate font-bold">{selected.title}</span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon
                 className="h-5 w-5 text-gray-400"
@@ -31,9 +34,9 @@ export default function QuestionSelect() {
             leaveTo="opacity-0"
           >
             <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-              {questions.map((question, questionIdx) => (
+              {questions.map((question) => (
                 <Listbox.Option
-                  key={questionIdx}
+                  key={question.id}
                   className={({ active }) =>
                     `relative cursor-default select-none py-2 px-4 ${
                       active ? "bg-dark-gray-7 text-white" : "text-gray-900"
@@ -48,7 +51,7 @@ export default function QuestionSelect() {
                           selected ? "font-medium" : "font-normal"
                         }`}
                       >
-                        {question.name}
+                        {question.title}
                       </span>
                       {selected ? (
                         <span className="absolute inset-y-0 right-2 flex items-center pl-3 text-neutral-950">
