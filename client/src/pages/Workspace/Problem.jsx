ed2cec91c809b59f5619c090170ed5e5aecaea99
Ayush Mishra <awesomelogix@gmail.com>
2023-08-13 13:44:42 +0530

    fix sizing bug

--- client/src/pages/Workspace/Problem.jsx
+++ client/src/pages/Workspace/Problem.jsx
@@ -24,59 +24,57 @@ function Problem() {
       ) : (
         <div>
           <div className="flex pb-2  h-[calc(100vh-44px)]">
-            <div className="px-4 pt-2 pb-8 overflow-y-scroll">
+            <div className="px-4 pt-2 pb-8 overflow-y-scroll w-full">
               {/* Problem heading */}
-              <div className="w-full">
-                <div className="flex space-x-4">
-                  <div className="mr-2 text-xl text-white font-bold">
-                    1. {data?.title}
-                  </div>
-                  <div className="flex justify-center items-center text-olive bg-olive rounded-[21px] bg-opacity-[0.15] px-2.5 ">
-                    <div className={` text-xs font-medium uppercase`}>
-                      {data?.difficulty}
-                    </div>
+              <div className="flex space-x-4">
+                <div className="mr-2 text-xl text-white font-bold">
+                  1. {data?.title}
+                </div>
+                <div className="flex justify-center items-center text-olive bg-olive rounded-[21px] bg-opacity-[0.15] px-2.5 ">
+                  <div className={` text-xs font-medium uppercase`}>
+                    {data?.difficulty}
                   </div>
                 </div>
+              </div>
 
-                {/* Problem Statement(paragraphs) */}
-                <div className="text-white text-sm mt-4">
-                  {data?.problem_statement}
-                </div>
+              {/* Problem Statement(paragraphs) */}
+              <div className="text-white text-sm mt-4">
+                {data?.problem_statement}
+              </div>
 
-                {/* Examples */}
-                <div className="mt-4">
-                  {data?.examples.map((example, id) => (
-                    <div key={id}>
-                      <p className="font-medium text-white ">
-                        Example {id + 1}:{" "}
-                      </p>
-                      <div className="example-card bg-dark-layer-2 text-white rounded-md">
-                        <pre>
-                          <strong className="text-white">Input: </strong>{" "}
-                          {example.input} <br />
-                          <strong>Output: </strong>
-                          {example.output} <br />
-                          {/* <strong>Explanation: </strong>Because nums[0] +
-                          nums[1] == 9, we return [0, 1]. */}
-                        </pre>
-                      </div>
+              {/* Examples */}
+              <div className="mt-4">
+                {data?.examples.map((example, id) => (
+                  <div key={id}>
+                    <p className="font-medium text-white ">
+                      Example {id + 1}:{" "}
+                    </p>
+                    <div className="example-card bg-dark-layer-2 text-white rounded-md">
+                      <pre>
+                        <strong className="text-white">Input: </strong>{" "}
+                        {example.input} <br />
+                        <strong>Output: </strong>
+                        {example.output} <br />
+                        {/* <strong>Explanation: </strong>Because nums[0] +
+                        nums[1] == 9, we return [0, 1]. */}
+                      </pre>
                     </div>
+                  </div>
+                ))}
+              </div>
+
+              {/* Constraints */}
+              <div className="my-5">
+                <div className="text-white text-sm font-medium">
+                  Constraints:
+                </div>
+                <ul className="text-white ml-5 list-disc">
+                  {data?.constraints.map((contraint, id) => (
+                    <li className="mt-2" key={id}>
+                      <code>{contraint}</code>
+                    </li>
                   ))}
-                </div>
-
-                {/* Constraints */}
-                <div className="my-5">
-                  <div className="text-white text-sm font-medium">
-                    Constraints:
-                  </div>
-                  <ul className="text-white ml-5 list-disc">
-                    {data?.constraints.map((contraint, id) => (
-                      <li className="mt-2" key={id}>
-                        <code>{contraint}</code>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+                </ul>
               </div>
             </div>
           </div>
