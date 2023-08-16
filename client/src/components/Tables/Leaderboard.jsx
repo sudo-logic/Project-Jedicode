8f05b19bcaad65cf12ff75e13fa5585245a60145
Ayush Mishra <awesomelogix@gmail.com>
2023-08-16 21:42:31 +0530

    add get leaderboard route

--- client/src/components/Tables/Leaderboard.jsx
+++ client/src/components/Tables/Leaderboard.jsx
@@ -43,50 +43,6 @@ const Leaderboard = () => {
               1500
             </td>
           </tr>
-          <tr className="transition-colors duration-300 hover:bg-slate-50 bg-black">
-            <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-400 stroke-slate-500 text-slate-100 ">
-              2
-            </td>
-            <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-400 stroke-slate-500 text-slate-100 ">
-              Mahesh
-            </td>
-            <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-400 stroke-slate-500 text-slate-100 ">
-              1000
-            </td>
-          </tr>
-          <tr className="transition-colors duration-300 hover:bg-slate-50 bg-black">
-            <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-400 stroke-slate-500 text-slate-100">
-              3
-            </td>
-            <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-400 stroke-slate-500 text-slate-100">
-              Beetlejuice
-            </td>
-            <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-400 stroke-slate-500 text-slate-100">
-              500
-            </td>
-          </tr>
-          <tr className="transition-colors duration-300 hover:bg-slate-50 bg-black">
-            <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-400 stroke-slate-500 text-slate-100 ">
-              4
-            </td>
-            <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-400 stroke-slate-500 text-slate-100 ">
-              Lambergamber
-            </td>
-            <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-400 stroke-slate-500 text-slate-100 ">
-              250
-            </td>
-          </tr>
-          <tr className="transition-colors duration-300 hover:bg-slate-50 bg-black">
-            <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-400 stroke-slate-500 text-slate-100">
-              5
-            </td>
-            <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-400 stroke-slate-500 text-slate-100">
-              Rahul Gandhi
-            </td>
-            <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-400 stroke-slate-500 text-slate-100">
-              100
-            </td>
-          </tr>
         </tbody>
       </table>
     </div>
