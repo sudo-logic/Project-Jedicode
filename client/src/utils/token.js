b133666484caa056d1a4755991cddf6ea9318b11
Ayush Mishra <awesomelogix@gmail.com>
2023-08-18 14:15:32 +0530

    fixed login signup bug

---
+++ client/src/utils/token.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function useToken() {
+  const getToken = () => {
+    const userToken = localStorage.getItem("token");
+    return userToken || null;
+  };
+
+  const [token, setToken] = useState(getToken());
+
+  const saveToken = (userToken) => {
+    localStorage.setItem("token", userToken);
+    setToken(userToken);
+    console.log(userToken)
+  };
+
+  return {
+    setToken: saveToken,
+    token,
+  };
+}
