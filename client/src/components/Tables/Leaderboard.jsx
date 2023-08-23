79bedda9da52e27a2895810ace6a74a013124ee9
Sitanshu Pokalwar <sitz2309@gmail.com>
2023-08-23 14:30:57 +0530

    leaderboard ui fix

--- client/src/components/Tables/Leaderboard.jsx
+++ client/src/components/Tables/Leaderboard.jsx
@@ -13,31 +13,31 @@ const Leaderboard = () => {
 
   return (
     <div className="w-full rounded">
-      <h1 className="text-lg text-slate-100 tracking-widest text-center mb-2 font-sans">
+      <h1 className="text-lg text-slate-100 tracking-widest uppercase text-center mb-2 font-sans">
         Leaderboard Table
       </h1>
 
       <table
-        className="w-full text-left border border-separate rounded overflow-y-auto border-slate-200 shadow-md shadow-[#5656ff]"
+        className="w-full text-center border border-separate rounded overflow-y-auto border-slate-200 shadow-md shadow-[#5656ff]"
         cellSpacing="0"
       >
         <tbody>
           <tr>
             <th
               scope="col"
-              className="h-12 px-6 text-sm font-medium border-l first:border-l-0 stroke-slate-700 text-slate-700 bg-slate-200"
+              className="h-12 px-6 text-sm font-bold border-l first:border-l-0 stroke-slate-700 text-slate-700 bg-slate-200"
             >
               Rank
             </th>
             <th
               scope="col"
-              className="h-12 px-6 text-sm font-medium border-l first:border-l-0 stroke-slate-700 text-slate-700 bg-slate-200"
+              className="h-12 px-6 text-sm font-bold border-l first:border-l-0 stroke-slate-700 text-slate-700 bg-slate-200"
             >
               Name
             </th>
             <th
               scope="col"
-              className="h-12 px-6 text-sm font-medium border-l first:border-l-0 stroke-slate-700 text-slate-700 bg-slate-200"
+              className="h-12 px-6 text-sm font-bold border-l first:border-l-0 stroke-slate-700 text-slate-700 bg-slate-200"
             >
               Score
             </th>
@@ -45,7 +45,7 @@ const Leaderboard = () => {
           {leaderboardData.map((user, index) => (
             <tr
               key={user.id}
-              className="transition-colors duration-300 hover:bg-slate-50 bg-black"
+              className="transition-colors duration-300 hover:bg-dark-layer-2 bg-black"
             >
               <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-400 stroke-slate-500 text-slate-100">
                 {index + 1}
