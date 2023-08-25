22a5161b5dcd89a9bb81a4c4fb5ae6ca78208310
Sitanshu Pokalwar <sitz2309@gmal.com>
2023-08-25 22:11:48 +0530

    font weight fix

--- client/src/components/Lobby.jsx
+++ client/src/components/Lobby.jsx
@@ -17,12 +17,12 @@ export default function Lobby() {
         <div className="w-full max-w-md transform  rounded-md bg-white  text-left align-middle  transition-all">
           <div className="bg-gray-100 rounded-md p-4">
             <div className="flow-root rounded-md px-2  py-2 transition duration-150 ease-in-out ">
-              <span className="text-lg font-extrabold text-neutral-950 ">
+              <span className="text-xl font-extrabold text-neutral-950 ">
                 Connected Jedi Knights
               </span>
 
-              <span className="block text-sm text-neutral-500">
-                <strong>Jedi Order:</strong> Knights sparring session
+              <span className="block font-medium text-sm text-neutral-500">
+                Jedi Order: Knights sparring session
               </span>
             </div>
           </div>
