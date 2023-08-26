4081ad510d9d2fa364dd87c2b60d882501130822
Ayush Mishra <awesomelogix@gmail.com>
2023-08-26 21:59:24 +0530

    add room_id in all submissions

--- server/src/modules/submissions/dtos/createSubmission.dto.ts
+++ server/src/modules/submissions/dtos/createSubmission.dto.ts
@@ -1,16 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsNumber, IsUUID } from 'class-validator';
 
 export class CreateSubmissionDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'a,b=map(int,input().split());print(a+b+1)',
+    description: 'The code to be executed',
+  })
   code: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'Python',
+    description: 'The language of the code to be executed',
+  })
   language: string;
 
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 71,
+    description: 'The score of the submission',
+  })
   language_id: number;
 
   // @IsNumber()
@@ -23,5 +36,17 @@ export class CreateSubmissionDto {
 
   @IsUUID()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'c2db2b49-8794-40c0-9eb6-d54c9a44d27a',
+    description: 'The question id',
+  })
   question_id: string;
+
+  @IsUUID()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: '57a5398d-487c-4bc0-89cd-970339032b3a',
+    description: 'The room id',
+  })
+  room_id: string;
 }
--- server/src/modules/submissions/submissions.entity.ts
+++ server/src/modules/submissions/submissions.entity.ts
@@ -2,6 +2,7 @@ import { Entity, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { User } from '../users/user.entity';
 import { Question } from '../questions/question.entity';
 import { AbstractEntity } from '../shared/abstract.entity';
+import { Room } from '../rooms/rooms.entity';
 
 @Entity()
 export class Submission extends AbstractEntity {
@@ -13,11 +14,18 @@ export class Submission extends AbstractEntity {
   @JoinColumn({ name: 'question_id' })
   question: Question;
 
+  @ManyToOne(() => Room, (room) => room.submissions)
+  @JoinColumn({ name: 'room_id' })
+  room: Room;
+
   @Column()
   user_id: string;
 
   @Column()
   question_id: string;
+
+  @Column()
+  room_id: string;
 
   @Column()
   code: string;
