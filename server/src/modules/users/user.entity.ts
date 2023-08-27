14c87c09578e2d01110fb3593afb34e6a942014c
Ayush Mishra <awesomelogix@gmail.com>
2023-08-28 04:06:22 +0530

    dynamic user score calc

--- server/src/modules/users/user.entity.ts
+++ server/src/modules/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, ManyToMany, JoinTable, OneToMany } from 'typeorm';
+import {
+  Entity,
+  Column,
+  ManyToMany,
+  JoinTable,
+  OneToMany,
+  AfterLoad,
+} from 'typeorm';
 import { Submission } from '../submissions/submissions.entity';
 
 import { AbstractEntity } from '../shared/abstract.entity';
@@ -15,13 +22,27 @@ export class User extends AbstractEntity {
   @Column({ select: false })
   password: string;
 
-  @Column({ default: 0 })
-  score: number;
+  // @Column({ default: 0 })
+  // score: number;
 
-  @OneToMany(() => Submission, (submission) => submission.user, {})
+  @OneToMany(() => Submission, (submission) => submission.user, { eager: true })
   submissions: Submission[];
 
   @ManyToMany(() => Question, (question) => question.id, {})
   @JoinTable()
   questions_attempted: Question[];
+
+  score: number;
+
+  @AfterLoad()
+  calculateScore() {
+    if (this.submissions) {
+      this.score = this.submissions.reduce(
+        (acc, submission) => acc + submission.score,
+        0,
+      );
+    } else {
+      this.score = 0;
+    }
+  }
 }
