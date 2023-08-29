2b3d3b12ebe942d380aa27731394fa8a187e6715
Ayush Mishra <awesomelogix@gmail.com>
2023-08-30 00:32:21 +0530

    server: update swagger api definitions

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -10,17 +10,19 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from './auth.guard';
 import { SignInDto } from './dtos/signin.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Public } from './public.decorator';
 import { SignUpDto } from './dtos/signup.dto';
 import { AuthService } from './auth.service';
 
 @ApiBearerAuth('access-token')
-@Controller('auth')
 @ApiTags('Auth')
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Sign in' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'User signed in successfully' })
   @HttpCode(HttpStatus.OK)
   @Post('login')
   @Public()
@@ -28,6 +30,8 @@ export class AuthController {
     return await this.authService.signIn(signInDto);
   }
 
+  @ApiOperation({ summary: 'Sign up' })
+  @ApiResponse({ status: HttpStatus.CREATED, description: 'User signed up successfully' })
   @Post('signup')
   @Public()
   async signUp(
@@ -36,6 +40,8 @@ export class AuthController {
     return await this.authService.signUp(signUpDto);
   }
 
+  @ApiOperation({ summary: 'Get user profile' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'User profile retrieved successfully' })
   @UseGuards(AuthGuard)
   @Get('profile')
   async getProfile(@Request() req) {
