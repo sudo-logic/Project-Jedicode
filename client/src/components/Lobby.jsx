589b0780fe8c12bb1a55ad31af6848417999faeb
Sitanshu Pokalwar <sitz2309@gmail.com>
2023-08-31 07:40:04 +0530

    fix room color

--- client/src/components/Lobby.jsx
+++ client/src/components/Lobby.jsx
@@ -5,14 +5,13 @@ import { useLocation, Navigate } from "react-router-dom";
 export default function Lobby() {
   const location = useLocation();
 
-
   function inviteMail() {}
 
   return (
     <>
       <div className="flex min-h-full items-center  justify-center p-4 text-center">
         <div className="w-full max-w-md transform  rounded-md bg-white  text-left align-middle  transition-all">
-          <div className="bg-gray-100 rounded-md p-4">
+          <div className="bg-gray-100 rounded-t-md p-4">
             <div className="flow-root rounded-md px-2  py-2 transition duration-150 ease-in-out ">
               <span className="text-xl font-extrabold text-neutral-950 ">
                 Connected Jedi Knights
