4963367deb38e836c308f1e081038ae0b7ac81eb
Sitanshu Pokalwar <sitz2309@gmal.com>
2024-05-12 01:12:51 +0530

    fix: result page layout

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -7,17 +7,15 @@ const Results = () => {
   return (
     <>
       <Navbar />
-      <div className="p-10 grid grid-cols-4 justify-center items-start gap-16 h-full bg-black">
+      <div className="p-8 w-full flex justify-between gap-8 h-full bg-black">
         {/* war options */}
-        <div className="col-span-3">
+        <div className="basis-[70%]">
           <ResultTable />
         </div>
 
         {/* leaderboard and war history */}
-        <div className="grid grid-rows-1 flex-col justify-center content-center gap-5">
-          <div className="">
-            <Leaderboard />
-          </div>
+        <div className="basis-[30%]">
+          <Leaderboard />
         </div>
       </div>
     </>
